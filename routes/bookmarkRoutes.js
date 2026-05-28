efaa3ca7fe9ea30d1662a463dc634490e5e159fb
Monica Davila <monica.davila510@gmail.com>
2026-05-28 09:06:06 -0500

    Added bookmark CRUD routes and auth middleware

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,9 +1,76 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Bookmark routes working",
-  });
+const Bookmark = require("../models/Bookmark");
+
+// CREATE bookmark
+router.post("/", async (req, res) => {
+  try {
+    const bookmark = await Bookmark.create(req.body);
+
+    res.status(201).json(bookmark);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+// GET all bookmarks
+router.get("/", async (req, res) => {
+  try {
+    const bookmarks = await Bookmark.find();
+
+    res.json(bookmarks);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+// GET one bookmark
+router.get("/:id", async (req, res) => {
+  try {
+    const bookmark = await Bookmark.findById(req.params.id);
+
+    res.json(bookmark);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+// UPDATE bookmark
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedBookmark = await Bookmark.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updatedBookmark);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+// DELETE bookmark
+router.delete("/:id", async (req, res) => {
+  try {
+    await Bookmark.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "Bookmark deleted",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = router;
